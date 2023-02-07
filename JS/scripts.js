571811302c15872ab7e7930964fd4c0ef7ed9e17
Maya-Bitter <mayabitter@gmail.com>
2023-02-07 13:00:31 +0100

    Updated a pokemonlist array

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,6 +1,13 @@
-alert('Hello world');
+let pokemonList = [
+{name: 'Bulbasaur', type: ['Grass', 'Poison'], Category: 'Seed'}, 
+{name:'Ivysaur', type: ['Grass', 'Poison'], Category: 'Seed'}, 
+{name: 'Venusaur', type: ['Grass', 'Poison'], Category: 'Seed'},  
+{name:'Charmander', type:'fire', Category: 'Lizard'},   
+{name: 'Charmeleon', type: 'fire', Category: 'Flame'},  
 
-let favouriteFood = 'Vegan Pizza';
-document.write(favouriteFood);
+];
+
+console.log(pokemonList);
+
 
 
