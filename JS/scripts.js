46336092d9b079d0ab24fab63b68a251af86d7bc
Maya-Bitter <mayabitter@gmail.com>
2023-02-08 13:21:50 +0100

    updated pokemon heights

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,13 +1,12 @@
 let pokemonList = [
-{name: 'Bulbasaur', type: ['Grass', 'Poison'], Category: 'Seed'}, 
-{name:'Ivysaur', type: ['Grass', 'Poison'], Category: 'Seed'}, 
-{name: 'Venusaur', type: ['Grass', 'Poison'], Category: 'Seed'},  
-{name:'Charmander', type:'fire', Category: 'Lizard'},   
-{name: 'Charmeleon', type: 'fire', Category: 'Flame'},  
+{name: 'Bulbasaur', type: ['Grass', 'Poison'], height: '2.04', Category: 'Seed'}, 
+{name:'Ivysaur', type: ['Grass', 'Poison'], height: '3.03', Category: 'Seed'}, 
+{name: 'Venusaur', type: ['Grass', 'Poison'], height: '6.07', Category: 'Seed'},  
+{name:'Charmander', type:'fire', height: '2', Category: 'Lizard'},   
+{name: 'Charmeleon', type: 'fire', height: '3.07', Category: 'Flame'},  
 
 ];
 
-console.log(pokemonList);
 
 
 
