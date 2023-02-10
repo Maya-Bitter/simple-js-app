dadfa3754ee736f04d1664ba7bba4a6d7a359915
Maya-Bitter <mayabitter@gmail.com>
2023-02-10 09:06:43 +0100

    added - to the text

--- JS/scripts.js
+++ JS/scripts.js
@@ -18,13 +18,13 @@ let pokemonList = [
   for (let i = 0; i <pokemonList.length; i++) { 
 
   if (pokemonList[i].height>=5){
-  document.write("<p>" + pokemonList[i].name + " " + pokemonList[i].height + " " + " Wow, that is big!")}
+  document.write("<p>" + pokemonList[i].name + " " + pokemonList[i].height + " " + "-  Wow, that is big!")}
 
   else if (pokemonList[i].height>=4 && pokemonList[i].height<=3) {
-  document.write("<p>" + pokemonList[i].name + " " + pokemonList[i].height + " " + " That is a medium Pokemon!")}
+  document.write("<p>" + pokemonList[i].name + " " + pokemonList[i].height + " " + " -  That is a medium Pokemon!")}
 
   else(pokemonList[i].height>=2 && pokemonList[i].height<=1);{ 
-  document.write("<p>" + pokemonList[i].name + " " + pokemonList[i].height + " " + " That is a small Pokemon!")} 
+  document.write("<p>" + pokemonList[i].name + " " + pokemonList[i].height + " " + " - That is a small Pokemon!")} 
 
 }
   
