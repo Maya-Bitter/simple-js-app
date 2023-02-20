4b15979aa2c851e2bd759f53c9c67fc5ab812afb
Maya-Bitter <mayabitter@gmail.com>
2023-02-20 13:14:22 +0100

    add function loadList

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,35 +1,24 @@
   let pokemonRepository = (function () {
-  let pokemonList = [{name: 'Bulbasaur', type: ['Grass', 'Poison'], height: '5', Category: 'Seed'}, 
-  {name:'Ivysaur', type: ['Grass', 'Poison'], height: '7', Category: 'Seed'}, 
-  {name: 'Venusaur', type: ['Grass', 'Poison'], height: '10', Category: 'Seed'},  
-  {name:'Charmander', type:'fire', height: '3', Category: 'Lizard'},   
-  {name: 'Charmeleon', type: 'fire', height: '3', Category: 'Flame'},
-  {name:'Ivysaur', type: ['Grass', 'Poison'], height: '7', Category: 'Seed'},
-  {name:'Charizard ', type: ['fire', 'flying'], height: '5', Category: 'Flame'},
-  {name:'Squirtle ', type: 'Water', height: '1', Category: 'Tiny Turtle'},
-  {name:'Wartortle ', type: 'Water', height: '3', Category: 'Turtle'},
-  {name:'Blastoise  ', type: 'Water', height: '3', Category: 'Shellfish'},
-  {name:'Caterpie  ', type: 'Bug', height: '5', Category: 'warm'},
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-];
- 
-   function add(pokemon) {
+  function add(pokemon) {
 
    pokemonList.push(pokemon);
 
     }
  
-   function getAll() {
+  function getAll() {
 
     return pokemonList;
 
    }
 
-   function showDetails(pokemon) { 
+  function showDetails(pokemon) { 
     console.log(pokemon.name);
   }
   
-    function addListItem(pokemon) {
+  function addListItem(pokemon) {
 
     let pokemonList = document.querySelector(".pokemon-list");
     let listpokemon = document.createElement("li");
@@ -42,6 +31,25 @@
     listpokemon.appendChild(button);
     pokemonList.appendChild(listpokemon);
     
+    }
+
+    // promise function //
+
+    function loadList() {
+    return fetch(apiUrl).then(function (response) {
+    return response.json();
+    }).then(function (json) {
+    json.results.forEach(function(item) {
+      let pokemon = {
+      name: item.name,
+      detailsUrl: item.url
+      };
+      add(pokemon);
+      console.log(pokemon);
+      });
+      }).catch(function (e) {
+        console.error(e);
+    })
     }
 
     return {
@@ -62,10 +70,3 @@ pokemonRepository.getAll().forEach(function(pokemon) {
 pokemonRepository.addListItem(pokemon);
 });
 
-fetch('https://pokeapi.co/api/v2/pokemon/').then(function (response) { // sends a message to the specified API, requesting the list of Pokémon.//
-  return response.json(); // This returns a promise! When the promise is resolved, an HTTP response containing the list of Pokémon is passed to the response parameter in the callback function of the first .then() block.
-}).then(function (pokemonList) {
-  console.log(pokemonList); // The actual JSON response
-}).catch(function () {
-  // Error
-});
